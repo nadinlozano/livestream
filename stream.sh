45dfa18b47efe46fdda9d0d5bcefb8b2fc9a7cb8
nadin lozano <35315929+nadinlozano@users.noreply.github.com>
2019-08-07 08:33:48 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# Run avconv to stream the webcam's video to the RTMP server.
+# Run ffmpeg to stream audio to the RTMP server.
 
-avconv  -f video4linux2 \       # Webcam format goes in 
-        -s 320x176 \            # Small is big enough
-        -r "10" \               # Fixed framerate at 10fps, somehow this needs to be a string
-        -b 256k \               # Fixed bitrate
-        -i /dev/video0 \        # Webcam device
-        -vcodec libx264 \       # h264.1 video encoder
-        -preset ultrafast \     # Use the fastest encoding preset
-        -f flv \                # Flash video goes out
-        -an \                   # No audio!
+arecord -D plughw:1,0 | ffmpeg  -i pipe:0 \       # audio format goes in 
+        -c:a aac \            # Small is big enough
+        -ac 2 \               # Fixed framerate at 10fps, somehow this needs to be a string
+        -ar 48000 \           # Fixed bitrate
+        -tune zerolatency \   # Webcam device
+#        -vcodec libx264 \    # h264.1 video encoder
+        -preset ultrafast \   # Use the fastest encoding preset#
+        -f flv \              # Flash video goes out
+#       -an \                 # No audio!
         'rtmp://<RASPBERRY_IP>/live/<STREAM_NAME>'
