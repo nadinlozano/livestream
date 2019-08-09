0945dd0e9d10a1d9a0e8cdecb48d66684804e193
nadin lozano <35315929+nadinlozano@users.noreply.github.com>
2019-08-09 18:13:37 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,13 +2,4 @@
 
 # Run ffmpeg to stream audio to the RTMP server.
 
-arecord -D plughw:1,0 | ffmpeg  -i pipe:0 \       # audio format goes in 
-        -c:a aac \            # Small is big enough
-        -ac 2 \               # Fixed framerate at 10fps, somehow this needs to be a string
-        -ar 48000 \           # Fixed bitrate
-        -tune zerolatency \   # Webcam device
-#        -vcodec libx264 \    # h264.1 video encoder
-        -preset ultrafast \   # Use the fastest encoding preset#
-        -f flv \              # Flash video goes out
-#       -an \                 # No audio!
-        'rtmp://192.168.0.12/audio/radio'
+arecord -f cd -D plughw:1,0 | ffmpeg  -i pipe:0 -C:a aac -ac 2 -ar 48000 -preset ultrafast -tune zerolatency -f flv rtmp://192.168.0.12:1935/audio/radio
