e918af765addf45fb2371a7eac2fa8e06d62c214
nadin lozano <35315929+nadinlozano@users.noreply.github.com>
2019-10-13 17:19:33 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,4 +2,4 @@
 
 # Run ffmpeg to stream audio to the RTMP server.
 
-arecord -f cd -D plughw:1,0 | ffmpeg  -i pipe:0 -c:a aac -ac 2 -ar 48000 -preset ultrafast -tune zerolatency -f flv rtmp://localhost:1935/audio/radio
+arecord -f cd -D plughw:1,0 | ffmpeg  -i pipe:0 -c:a aac -ac 2 -b:a 128k -ar 48000 -preset ultrafast -tune zerolatency -f flv rtmp://localhost:1935/audio/radio
