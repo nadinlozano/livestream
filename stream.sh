e29ab42e77ea2c72385047624b452f527ab805c3
nadin vicente  lozano gomez <35315929+nadinlozano@users.noreply.github.com>
2020-07-07 15:35:08 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,4 +2,4 @@
 
 # Run ffmpeg to stream audio to the RTMP server.
 
-arecord -f cd -D sysdefault:1 | ffmpeg  -i pipe:0 -c:a aac -ac 2 -b:a 128k -r:a 48000 -f flv rtmp://localhost:1935/audio/radio
+ffmpeg -f s16le -i plughw:1,0 -c:a aac -ac 2 -b:a 128k -r:a 48000 -f flv rtmp://localhost:1935/audio/radio
